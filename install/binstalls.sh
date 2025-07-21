60168a938b44bc9310653a827ca263a306afa2ab
Blake Scampone <blake.scampone@gmail.com>
2025-07-21 15:54:16 -0700

    Added various bash + installs + tmux

---
+++ install/binstalls.sh
@@ -0,0 +1,6 @@
+# Install cargo binstall
+# curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+# cargo install cargo-binstall
+
+# install TV
+cargo binstall television
--- install/tmux.sh
+++ install/tmux.sh
@@ -2,4 +2,5 @@ yay -S tmux --noconfirm
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
-tmux source ~/.config/tmux/tmux.conf
+# tmux source ~/.config/tmux/tmux.conf
+# tmux new
