9f7f081c1ac9c4922f69c3a428b80ad3c7fb8b25
Blake Scampone <blake.scampone@gmail.com>
2025-07-25 03:58:10 -0700

    Added various changes

---
+++ install/pdf.sh
@@ -0,0 +1,4 @@
+yay -S zathura zathura-pdf-mupdf --noconfirm
+mkdir -p ~/.config/zathura
+echo "set selection-clipboard clipboard" > ~/.config/zathura/zathurarc
+# Keybinds: https://man.archlinux.org/man/zathura.1
--- install/starship.sh
+++ install/starship.sh
@@ -1,2 +1,10 @@
 curl -sS https://starship.rs/install.sh | sh
 # echo 'eval "$(starship init bash)"' >>.bashrc
+
+toml=$(cat <<EOF
+[character] # The name of the module we are configuring is 'character'
+success_symbol = '[λ](bold green)'
+error_symbol = '[λ](bold red)'
+EOF
+)
+echo "$toml" > ~/.config/starship.toml
